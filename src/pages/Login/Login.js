07a961f83c265f689e3a79e7b200c095a24e83bc
Adrianus Vieira <adrianusvieira@cpejr.com.br>
2023-01-08 18:44:15 -0300

    feat:login funcional

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import { AuxiliarText, Base, Body, Box, InitialTitle } from "./Styles";
 import Input from "../../Styles/Input";
 import StartButton from "../../Styles/StartButton";
 
+import * as managerService from "../../services/managerService";
+
 function Login() {
   const history = useHistory();
+  const [email, setEmail] = useState("");
+
+  async function getUsuarioByEmail() {
+    const result = await managerService.getUsuarioByEmail(email);
+    if (result) {
+      history.push("/home");
+    } else {
+      window.location.reload();
+    }
+  }
+
   return (
     <Body>
       <Base>
@@ -15,8 +28,13 @@ function Login() {
             width="70%"
             marginTop="30px"
             placeholder="Insira seu email jogador:"
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
           />
-          <StartButton marginTop="30px">Start</StartButton>
+          <StartButton onClick={() => getUsuarioByEmail()} marginTop="30px">
+            Start
+          </StartButton>
           <AuxiliarText
             onClick={() => {
               history.push("/cadastro");
