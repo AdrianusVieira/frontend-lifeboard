8124f7e1bd3bb1be86f68ae590f4f634a6e01878
Adrianus Vieira <adrianusvieira@cpejr.com.br>
2023-01-08 16:50:54 -0300

    feat: front cadastro

--- src/pages/Cadastro/Cadastro.js
+++ src/pages/Cadastro/Cadastro.js
@@ -1,11 +1,36 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+import { AuxiliarText, Base, Body, Box, InitialTitle } from "./Styles";
+import Input from "../../Styles/Input";
+import StartButton from "../../Styles/StartButton";
 
 function Cadastro() {
   return (
-    <div>
-      <h1>Cadastro</h1>
-    </div>
+    <Body>
+      <Base>
+        <Box>
+          <InitialTitle>Welcome to Board!</InitialTitle>
+          <AuxiliarText>Favor Preencher:</AuxiliarText>
+          <Input
+            width="60%"
+            marginTop="30px"
+            placeholder="Nome Completo:"
+          />
+          <Input
+            width="60%"
+            marginTop="30px"
+            placeholder="Endereço de Email:"
+          />
+          <Input
+            width="60%"
+            marginTop="30px"
+            placeholder="Data de Nascimento:"
+          />
+          <StartButton marginTop="30px">Start</StartButton>
+        </Box>
+      </Base>
+    </Body>
   );
 }
 
-export default Cadastro
+export default Cadastro;
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,21 +1,29 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import { AuxiliarText, Base, Body, Box, InitialTitle } from "./Styles";
 import Input from "../../Styles/Input";
 import StartButton from "../../Styles/StartButton";
 
 function Login() {
+  const history = useHistory();
   return (
     <Body>
       <Base>
         <Box>
           <InitialTitle>Lets Begin!</InitialTitle>
           <Input
-            width="80%"
+            width="70%"
             marginTop="30px"
             placeholder="Insira seu email jogador:"
           />
           <StartButton marginTop="30px">Start</StartButton>
-          <AuxiliarText>Ainda não Joga ?</AuxiliarText>
+          <AuxiliarText
+            onClick={() => {
+              history.push("/cadastro");
+            }}
+          >
+            Ainda não Joga ?
+          </AuxiliarText>
         </Box>
       </Base>
     </Body>
